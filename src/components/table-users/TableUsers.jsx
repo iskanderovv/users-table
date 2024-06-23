79f5939b19f34f35cb483ee1da175f7c944c4dca
Akbar Iskanderov <iskanderovofficial@gmail.com>
2024-06-23 12:45:21 +0500

    responsive qilindi

--- src/components/table-users/TableUsers.jsx
+++ src/components/table-users/TableUsers.jsx
@@ -6,42 +6,45 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
 
 
 export default function TableUsers({ users }) {
     console.log(users);
     return (
-        <Container maxWidth='xl' sx={{mt: '50px'}}>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>ID</TableCell>
-                            <TableCell>Name</TableCell>
-                            <TableCell>City</TableCell>
-                            <TableCell>Email</TableCell>
-                            <TableCell>Phone</TableCell>
-                            <TableCell>Company</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {users?.map((user) => (
-                            <TableRow
-                                key={user.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">{user.id}</TableCell>
-                                <TableCell>{user.name}</TableCell>
-                                <TableCell>{user.address.city}</TableCell>
-                                <TableCell>{user.email}</TableCell>
-                                <TableCell>{(user.phone).split(" ")[0]}</TableCell>
-                                <TableCell>{user.company.name}</TableCell>
+        <Box sx={{ width: '100%', overflowX: 'auto' }}>
+            <Container maxWidth='xl' sx={{ mt: '50px' }}>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>ID</TableCell>
+                                <TableCell>Name</TableCell>
+                                <TableCell>City</TableCell>
+                                <TableCell>Email</TableCell>
+                                <TableCell>Phone</TableCell>
+                                <TableCell>Company</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Container>
+                        </TableHead>
+                        <TableBody>
+                            {users?.map((user) => (
+                                <TableRow
+                                    key={user.id}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">{user.id}</TableCell>
+                                    <TableCell>{user.name}</TableCell>
+                                    <TableCell>{user.address.city}</TableCell>
+                                    <TableCell>{user.email}</TableCell>
+                                    <TableCell>{(user.phone).split(" ")[0]}</TableCell>
+                                    <TableCell>{user.company.name}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Container>
+        </Box>
+
     );
 }
